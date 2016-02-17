6b5ca686d1aaa61ee41f4ba971db95d3dd50ca67
Francis Castro <fgcastro@wpi.edu>
2016-02-17 15:14:20 -0500

    Updated directories - deleted unnecessary files

--- Code/problem-set/Program Solutions - mjri/isPalindrome.js
+++
@@ -1,7 +0,0 @@
-// function that accepts one parameter, a string
-function isPalindrome(string) {
-	// remove all characters that are alpha characters ([a-zA-Z]), then transforms string to lowercase
-	string = string.replace(/[^a-zA-Z]/g, '').toLowerCase();
-	// returns the result of palindrome checker
-	return string == string.split('').reverse().join('');
-}
