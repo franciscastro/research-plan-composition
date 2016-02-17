4113c8d7b149d090033b0787681ddb18225212b5
Francis Castro <fgcastro@wpi.edu>
2016-02-17 16:17:27 -0500

    Uploaded R file for data analysis

---
+++ analysis/analysis.r
@@ -0,0 +1,26 @@
+# Author: Francisco Castro (fgcastro@wpi.edu)
+# Last modified: 17 February 2016
+
+# Notes: Pre-load the following packages
+# library(tidyr)
+# library(dplyr)
+# library(ggplot2)
+
+# Set working directory for file manipulation
+working_dir <- "C:/Git Repositories/files"
+setwd(working_dir)
+getwd()
+
+
+# Set file names
+file_name <- "wpi-id-key.csv"
+
+
+# Read file
+stud_data <- read.csv(file_name)
+
+
+# Get data information
+str(stud_data)
+summary(stud_data)
+
