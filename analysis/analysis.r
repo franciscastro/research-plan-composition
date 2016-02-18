293bcd2fee3ab799f509e9f8b48922eec563acef
Francis Castro <fgcastro@wpi.edu>
2016-02-18 02:43:57 -0500

    Updated R analysis file with sampling groups

--- analysis/analysis.r
+++ analysis/analysis.r
@@ -46,14 +46,32 @@ summary(stud_data)
 ##################################################
 
 # Data frame with username and prior_course
-stud_prior_course <- stud_data[,c("username", "prior_course")]
+stud_prior <- stud_data[,c("username", "prior_course")]
 
-# Generate data frame of prior_course and its frequency count
-prior_course_freqs <- count_(stud_data, "prior_course", sort = TRUE)
-prior_course_freqs <- as.data.frame(prior_course_freqs)
-colnames(prior_course_freqs)[2] <- "frequency"
+# Data frame with username, course_grade, jp_score, and prior_course
+stud_jp_prior <- stud_data[,c("username", "course_grade", "jp_score", "prior_course")]
 
-# Data frame with username and prior_course
+# Generate data frame of prior_courses and their frequency counts
+prior_course_freqs <- count_(stud_data, "prior_course", sort = TRUE)  # Get a table of counts of each prior_course
+prior_course_freqs <- as.data.frame(prior_course_freqs)  # Convert table to data frame
+colnames(prior_course_freqs)[2] <- "frequency"  # Rename column "n" to "frequency"
+
+# Generate data frame from stud_jp_prior with prior_course == "CS1101-none"
+cs1101_prior <- stud_jp_prior[stud_jp_prior$prior_course == "CS1101-none",]
+
+# Generate data frame of prior_course and frequency count containing "1102"
+prior_1102_freqs <- prior_course_freqs[grep(pattern = "1102", prior_course_freqs$prior_course),]
+
+# Generate data frame from stud_jp_prior with prior_course containing "1102"
+cs1102_prior <- stud_jp_prior[grep(pattern = "1102", stud_jp_prior$prior_course),]
+
+# Get data information
+str(stud_prior)
+str(stud_jp_prior)
+str(prior_course_freqs)
+str(cs1101_prior)
+str(prior_1102_freqs)
+str(cs1102_prior)
 
 ##################################################
 
